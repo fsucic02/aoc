60299418c84aa29e49f34e3d776317d4dd4f2e16
Filip Sučić <fs54014@fer.hr>
2024-12-14 14:07:13 +0100

    added day 14

---
+++ 2024/24_14/1.py
@@ -0,0 +1,27 @@
+import re
+from math import prod
+
+data = open('24_14/input.txt').read().splitlines()
+h, w = 102, 100
+
+robots = []
+for robot in data:
+    robot = list(map(int, re.findall(r'(-?\d+)', robot)))
+    p, v = robot[:2], robot[2:]
+
+    p[0] = (p[0] + 100 * v[0]) % (w + 1)
+    p[1] = (p[1] + 100 * v[1]) % (h + 1)
+    
+    robots.append((p, v))
+
+quadrants = [
+    [0, 0],
+    [0, 0]
+]
+for robot in robots:
+    p1, p2 = robot[0]
+    if p1 == w / 2 or p2 == h / 2: continue
+
+    quadrants[int(p1 // ((w + 1)/ 2))][int(p2 // ((h + 1) / 2))] += 1
+    
+print(prod(sum(quadrants, [])))
---
+++ 2024/24_14/2.py
@@ -0,0 +1,24 @@
+import re
+
+data = open('24_14/input.txt').read().splitlines()
+h, w = 102, 100
+
+robots = []
+for robot in data:
+    robot = list(map(int, re.findall(r'(-?\d+)', robot)))
+    p, v = robot[:2], robot[2:]
+    
+    robots.append((p, v))
+
+for second in range(1, 10_000):
+    seen = set()
+    for i, robot in enumerate(robots):
+        p, v = robot
+        p[0] = (p[0] + v[0]) % (w + 1)
+        p[1] = (p[1] + v[1]) % (h + 1)
+        seen.add(tuple(p))
+    
+    if len(seen) == len(data):
+        break
+
+print(second)
